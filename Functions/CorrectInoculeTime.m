16b9d99403127a5671632957a39322b1ba83ce06
AbrahamAvelar <jabrahamavelar@gmail.com>
2018-05-11 12:29:07 -0500

    Update CorrectInoculeTime.m

--- Functions/CorrectInoculeTime.m
+++ Functions/CorrectInoculeTime.m
@@ -1,5 +1,16 @@
 function BgDataAll = CorrectInoculeTime(BgDataAll, platos, odTh, ExcelHorasInoculos )
 % BgDataAll = CorrectInoculeTime(BgDataAll, odTh, ExcelHorasInoculos )
+% ExcelHorasInoculos = 'C:\Users\JAbraham\Dropbox\HorasInoculosE2Ago16.xlsx';
+% con 3 columnas, Col1 con numero de plato, Col2 con dia Col3 con la hora
+% por ejemplo:
+%
+%   1   09-ago  21:25
+%   2   09-ago  21:27
+%   3   09-ago  21:30
+%   1   10-ago  19:05
+%   2   10-ago  19:06
+%   3   10-ago  19:35
+%
 % odTh=.3;%cu'anto tiene que bajar la od en promedio para que sea considerado otro dia
 horasInoculos=ExcelHorasInoculos;
 [NUM TXT]= xlsread(horasInoculos);
