86557ff6bdd1c2382442bd3d03d7b6c882cb385a
AbrahamAvelar <jabrahamavelar@gmail.com>
2018-08-23 12:30:37 -0500

    Update calculaTiempos.m

--- Functions/calculaTiempos.m
+++ Functions/calculaTiempos.m
@@ -5,7 +5,7 @@ function CleanData = calculaTiempos(CleanData, pls, odTh)
         NuevosDias=EncuentraDias(CleanData(pl), odTh);
         NuevosDias=[NuevosDias length(CleanData(pl).t)];
         for Dia=1:length(NuevosDias)-1
-            CleanData(pl).tOut=[CleanData(pl).tOut CleanData(pl).t(NuevosDias(Dia):NuevosDias(Dia+1)-1)-CleanData(pl).t(NuevosDias(Dia) )];
+            CleanData(pl).tOut=[CleanData(pl).tOut' CleanData(pl).t(NuevosDias(Dia):NuevosDias(Dia+1)-1)-CleanData(pl).t(NuevosDias(Dia) )'];
         end
         CleanData(pl).Tdays=CleanData(pl).t(NuevosDias)-CleanData(pl).t(1);
     end
